ab4b990ba50bfdbc26551a5c06d2fe2b5ffb8216
Victoria <167627607+VictoriaDoneva@users.noreply.github.com>
2024-06-01 21:22:13 +0300

    Add files via upload

---
+++ specialNumbers.js
@@ -0,0 +1,26 @@
+function specialNumbers(input) {
+    let n = Number(input[0]);
+    let result = "";
+
+    for(let curNum = 1111; curNum <= 9999; curNum++){
+        let isSpecial = true;
+        
+        let curNumAsStr = curNum.toString();
+
+        for( let i = 0; i < curNumAsStr.length; i++) {
+            let curDigit = Number(curNumAsStr[i]);
+            
+            if (n % curDigit !== 0) {
+                isSpecial = false;
+                break;
+            }
+        }
+        if (isSpecial) {
+            result += curNum + ` `;
+
+        }
+    }   
+console.log(result);
+}
+
+specialNumbers(["3"])
