1041946a968e7b7fc615a3b71716e8f790b49c32
Victoria <167627607+VictoriaDoneva@users.noreply.github.com>
2024-06-12 23:30:13 +0300

    Add files via upload

---
+++ passwordGuess.js
@@ -0,0 +1,10 @@
+function passwordGuess(input) {
+    let a = (input[0]);
+    
+    if (a === "s3cr3t!P@ssw0rd") {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong password!");
+    }
+}
+passwordGuess("s3cr3tfssw0rd")
