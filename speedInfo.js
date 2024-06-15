1b33f185c06f49d990a47ccee81bdfa8acbc1d08
Victoria <167627607+VictoriaDoneva@users.noreply.github.com>
2024-06-15 23:52:16 +0300

    Add files via upload

---
+++ speedInfo.js
@@ -0,0 +1,16 @@
+function speedInfo(input) {
+    let a = Number(input[0]);
+
+if (a <= 10) 
+    console.log("slow");
+else if (a <= 50)
+    console.log("average");
+else if (a <= 150)
+    console.log("fast");
+else if (a <= 1000)
+    console.log("ultra fast");
+else
+    console.log("extremely fast");
+  
+}
+speedInfo(["1001"])
