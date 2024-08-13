392a4d95712817e8e6ec39b9d5562881c6a51c66
Victoria <167627607+VictoriaDoneva@users.noreply.github.com>
2024-08-13 22:00:52 +0300

    Add files via upload

---
+++ personalTitles.js
@@ -0,0 +1,19 @@
+function personalTitles(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if (age < 16) {
+        if (gender === "m") {
+        console.log("Master");
+    } else {
+        console.log("Miss");
+    }
+} else {
+    if (gender === "f") {
+        console.log("Ms.");
+    } else {
+        console.log("Mr.");
+    }
+}
+}
+personalTitles(["20", "m"]);
