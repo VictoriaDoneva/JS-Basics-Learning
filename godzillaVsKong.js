7e22ed5216813dd83bc594a0cf5dafdd964cb0d7
Victoria <167627607+VictoriaDoneva@users.noreply.github.com>
2025-01-22 15:44:42 +0200

    Add files via upload

--- godzillaVsKong.js
+++ godzillaVsKong.js
@@ -1,24 +1,24 @@
 function godzillaVsKong(input) {
-    let movieBudjet = Number(input[0]);
-    let statNumber = Number(input[1]);
-    let clothingPrice = Number(input[2]);
+    let movieBudget = Number(input[0]);
+    let countStats = Number(input[1]);
+    let suitPerOneStat = Number(input[2]);
 
-    let decorPrice = movieBudjet * 0.10;
-    let totalClothing = statNumber * clothingPrice;
+    let decor =  movieBudget * 0.10;
     
-if (statNumber > 150) {
-    totalClothing *= 0.90;
-}
+    if (countStats > 150) {
+        suitPerOneStat = suitPerOneStat * 0.90;
+    }
 
-let moviePrice = decorPrice + totalClothing;
+    let budgetCheck = countStats * suitPerOneStat + decor;
 
-if (movieBudjet >= moviePrice) {
-    console.log (`Action!`);
-    console.log(`Wingard starts filming with ${(movieBudjet - moviePrice).toFixed(2)} leva left.`)
-} else {
-    console.log(`Not enough money!`);
-    console.log(`Wingard needs ${(moviePrice - movieBudjet).toFixed(2)} leva more.`);
+    if (movieBudget >= budgetCheck) {
+        console.log(`Action!`);
+        console.log(`Wingard starts filming with ${(movieBudget - budgetCheck).toFixed(2)} leva left.`);
+    } else {
+        console.log(`Not enough money!`);
+        console.log(`Wingard needs ${(budgetCheck - movieBudget).toFixed(2)} leva more.`);
+    }
 }
-}
-
-godzillaVsKong(["9587.88", "222", "55.68"])
+godzillaVsKong(["9587.88",
+"222",
+"55.68"])
