11b09e9d52088862c0d8beb3388453de69edd705
Victoria <167627607+VictoriaDoneva@users.noreply.github.com>
2025-03-16 18:57:28 +0200

    Add files via upload

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,10 +1,8 @@
-function multiplicationTable(input) {
-    let n = Number(input[0]);
-
-    for (i = 1; i <= 10; i++) {
-        let res = i * n;
-        console.log(`${i} * ${n} = ${res}`)
-
+function multiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} X ${i} = ${number * i}`)
     }
 }
-multiplicationTable(["5"]);
+
+multiplicationTable(5);
+multiplicationTable(2);
